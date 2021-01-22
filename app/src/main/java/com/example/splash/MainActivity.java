f65fc5a7ebdb397ea124bf85e0ca309b5ab363ab
Emmanuel Lamprea <ejlf14@hotmail.com>
2021-01-22 08:07:01 -0500

    Initial commit

---
+++ app/src/main/java/com/example/splash/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT > 16) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+
+        setContentView(R.layout.activity_main);
+
+        TimerTask tarea = new TimerTask() {
+            @Override
+            public void run() {
+                Intent   intent = new Intent(MainActivity.this, Loginactivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        };
+
+        Timer tiempo = new Timer();
+
+        tiempo.schedule(tarea, 4000);
+    }
+
+
+}
