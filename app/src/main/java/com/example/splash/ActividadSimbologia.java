a86626269596af372042eddb0f78c5ab17f2ad66
wmartinezca <70444529+wmartinezca@users.noreply.github.com>
2021-01-24 12:17:38 -0500

    Creacion actividad simbolos

---
+++ app/src/main/java/com/example/splash/ActividadSimbologia.java
@@ -0,0 +1,15 @@
+package com.example.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActividadSimbologia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad_simbologia);
+    }
+
+}
