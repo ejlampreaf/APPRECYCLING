4a1b7dd0bc874263d06ad3eed7507c02152c396a
ejlampreaf <ejlf14@hotmail.com>
2021-01-25 15:13:16 -0500

    Añadí boton registro y opcion olvidé la contraseña, tambien añadí la activity de registro - Emmanuel

---
+++ app/src/main/java/com/example/splash/registroActivity.java
@@ -0,0 +1,29 @@
+package com.example.splash;
+
+import android.os.Build;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.WindowManager;
+
+public class registroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT > 16) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+
+        setContentView(R.layout.activity_registro);
+
+}
+}
