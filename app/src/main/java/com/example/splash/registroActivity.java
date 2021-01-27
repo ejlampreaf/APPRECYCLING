3fb42599d3894b3c747e099e7f21b2964dbde0b7
ejlampreaf <ejlf14@hotmail.com>
2021-01-27 15:42:29 -0500

    Modifiqué algunas cosas de diseño en el registro y añadí pantalla completa a la activity- Emmanuel

--- app/src/main/java/com/example/splash/registroActivity.java
+++ app/src/main/java/com/example/splash/registroActivity.java
@@ -1,15 +1,10 @@
 package com.example.splash;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.view.View;
 import android.view.WindowManager;
 
@@ -23,13 +18,11 @@ public class registroActivity extends AppCompatActivity {
             getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                     WindowManager.LayoutParams.FLAG_FULLSCREEN);
         }
-
         setContentView(R.layout.activity_registro);
     }
 
-    public void backl(View view){
-        Intent volverl = new Intent(this, Loginactivity.class);
-        startActivity(volverl);
+    public void already(View view){
+        Intent alcuenta = new Intent(this, Loginactivity.class);
+        startActivity(alcuenta);
     }
-    
 }
