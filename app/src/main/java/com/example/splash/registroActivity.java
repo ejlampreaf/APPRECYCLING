e625243354d492408a294487d4c9f122eeb8d805
ejlampreaf <ejlf14@hotmail.com>
2021-01-27 13:37:00 -0500

    Modifiqué algunas cosas de diseño en el inicio de sesion, registro y añadí una pantalla de confirmación de registro éxitoso- Emmanuel

--- app/src/main/java/com/example/splash/registroActivity.java
+++ app/src/main/java/com/example/splash/registroActivity.java
@@ -1,5 +1,6 @@
 package com.example.splash;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 
@@ -24,6 +25,11 @@ public class registroActivity extends AppCompatActivity {
         }
 
         setContentView(R.layout.activity_registro);
+    }
 
-}
+    public void backl(View view){
+        Intent volverl = new Intent(this, Loginactivity.class);
+        startActivity(volverl);
+    }
+    
 }
