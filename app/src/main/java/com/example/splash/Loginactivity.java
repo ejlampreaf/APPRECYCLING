2e3c8e5de6c10a3fbbff8531b1683d65186e1773
ejlampreaf <ejlf14@hotmail.com>
2021-01-28 12:55:46 -0500

    A la activity de inicio de sesion puse unos toats donde le indica al usuario que falta uno de los campos o ambos - Emmanuel

--- app/src/main/java/com/example/splash/Loginactivity.java
+++ app/src/main/java/com/example/splash/Loginactivity.java
@@ -7,8 +7,12 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Loginactivity extends AppCompatActivity {
+
+    private EditText n, p;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,23 @@ public class Loginactivity extends AppCompatActivity {
         }
 
         setContentView(R.layout.activity_loginactivity);
+
+
+        n = (EditText)findViewById(R.id.camponombreis);
+        p = (EditText)findViewById(R.id.campopasswordis);
+    }
+
+    public void ingresar (View view){
+        String nombre = n.getText().toString();
+        String contrasena = p.getText().toString();
+
+        if (nombre.length() == 0){
+            Toast.makeText(this, "Ingresa un nombre", Toast.LENGTH_LONG).show();
+        } if (contrasena.length() == 0){
+            Toast.makeText(this, "Ingresa una contraseña", Toast.LENGTH_LONG).show();
+        } if (nombre.length() != 0 && contrasena.length() != 0 ){
+            Toast.makeText(this, "Ingresando al home", Toast.LENGTH_LONG).show();
+        }
     }
 
     public void registrarme(View view){
