d1abc30456b87df6ea81305602c603f06c87b755
JORGE CARRILLO <Dikajo0812**>
2021-02-04 02:57:41 -0500

    Prueba Menu - Jorge carrillo

--- app/src/main/java/com/example/splash/MainActivity2.java
+++ app/src/main/java/com/example/splash/MainActivity2.java
@@ -4,22 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 
-public class InfoActivity extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_info);
+        setContentView(R.layout.activity_main2);
     }
 
     //Metodo para mostrar los botones de accion
     public boolean onCreateOptionsMenu (Menu menu){
-        getMenuInflater().inflate(R.menu.menuacciones, menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menuacciones, menu);
         return true;
     }
 
 
+
 }
-
-
