e7ef253c94be37628131beb983af12eb318872cd
DeinerA <deiner.ariza@globant.com>
2021-02-04 22:03:19 -0500

    nuevos objetos para creacion de lugares - Deiner Ariza

--- app/src/main/java/com/example/splash/Activity_busqueda_lugares.java
+++ app/src/main/java/com/example/splash/Activity_busqueda_lugares.java
@@ -1,14 +1,63 @@
 package com.example.splash;
-
+/* Codigo creado por Deiner Ariza*/
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class Activity_busqueda_lugares extends AppCompatActivity {
+
+    private ArrayList<localidad> listalocalidades;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_busqueda_lugares);
+
+        listalocalidades=new ArrayList<localidad>();
+        listalocalidades.add(new localidad("Suba"));
+
+        AdaptadorLocalidad adaptador = new AdaptadorLocalidad(this);
+        ListView list1 = (ListView)findViewById(R.id.list1);
+        list1.setAdapter(adaptador);
     }
+
+    class AdaptadorLocalidad extends ArrayAdapter<localidad>{
+        AppCompatActivity appCompatActivity;
+        AdaptadorLocalidad(AppCompatActivity context){
+            super(context, R.layout_localidad, listalocalidades);
+            appCompatActivity = context;
+
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent){
+            LayoutInflater inflater = appCompatActivity.getLayoutInflater();
+            View item = inflater.inflate(R.layout.persona, null);
+
+            TextView textView1 = (ImageView)item.findViewById(R.id.textView);
+            textView1.setText(listalocalidades.get(position).getSuba());
+
+            ImageView imageView1 = (imageView)item.findViewById(R.id.imageView);
+            if (listalocalidades.get(position));
+                imageView1.setImageResource(R.mipmap.suba);
+            else
+                imageView1.setImageResource(R.mipmap.suba);
+            return(item);
+
+
+
+        }
+
+
+    }
+
 }
---
+++ app/src/main/java/com/example/splash/localidad.java
@@ -0,0 +1,18 @@
+package com.example.splash;
+/* Codigo creado por Deiner Ariza*/
+public class localidad {
+
+    private String n_localidad;
+
+    public localidad(String n_localidad){
+        this.n_localidad=n_localidad;
+    }
+    public String getSuba(){
+        return n_localidad
+    }
+
+
+}
+
+
+
