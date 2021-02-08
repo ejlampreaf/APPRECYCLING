f7e34968e7923bd5e18903cda16463f1f78cb88e
wmartinezca <70444529+wmartinezca@users.noreply.github.com>
2021-02-08 15:07:50 -0500

    se ajusta navegación del fragment home hacia nav_perfil y hacia miayuda y devuelta - William

--- app/src/main/java/com/example/splash/nav_Perfil.java
+++ app/src/main/java/com/example/splash/nav_Perfil.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -53,12 +55,20 @@ public class nav_Perfil extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nav__perfil, container, false);
+            return inflater.inflate(R.layout.fragment_nav__perfil, container, false);
+
+
+
     }
+
+
+
 }
