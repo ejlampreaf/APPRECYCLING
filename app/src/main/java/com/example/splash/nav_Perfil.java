ead183438da884a6b2c2a891f94e97e187426f97
wmartinezca <70444529+wmartinezca@users.noreply.github.com>
2021-02-14 18:32:30 -0500

    Creación logica pasar datos de modificar perfil a ver perfil - William

--- app/src/main/java/com/example/splash/nav_Perfil.java
+++ app/src/main/java/com/example/splash/nav_Perfil.java
@@ -2,6 +2,8 @@ package com.example.splash;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -18,6 +20,43 @@ import android.widget.EditText;
  * create an instance of this fragment.
  */
 public class nav_Perfil extends Fragment {
+    EditText IngNombre, IngEdad, IngSexo, IngCiudad, IngPensamiento;
+    Button btnGuardar;
+
+    public nav_Perfil() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInsatanceState) {
+        return inflater.inflate(R.layout.fragment_nav__perfil, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        IngNombre = view.findViewById(R.id.IngNombre);
+        IngEdad = view.findViewById(R.id.IngEdad);
+        IngSexo = view.findViewById(R.id.IngSexo);
+        IngCiudad = view.findViewById(R.id.IngCiudad);
+        IngPensamiento = view.findViewById(R.id.IngPensamiento);
+        btnGuardar = view.findViewById(R.id.btnGuardar);
+
+        btnGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle bundle = new Bundle();
+                bundle.putString("perfNombre",IngNombre.getText().toString().trim());
+                bundle.putString("perfEdad",IngEdad.getText().toString().trim());
+                bundle.putString("perfSexo",IngSexo.getText().toString().trim());
+                bundle.putString("perfCiudad",IngCiudad.getText().toString().trim());
+                bundle.putString("perfPensamiento",IngPensamiento.getText().toString().trim());
+
+                getParentFragmentManager().setFragmentResult( "Key", bundle);
+            }
+        });
+    }
+
 
 
 
--- app/src/main/java/com/example/splash/verPerfil.java
+++ app/src/main/java/com/example/splash/verPerfil.java
@@ -2,11 +2,15 @@ package com.example.splash;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentResultListener;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +18,61 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class verPerfil extends Fragment {
+    TextView perf_nombre, perf_edad, perf_sexo, perf_ciudad, perf_pensamiento;
+
+    public verPerfil() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getParentFragmentManager().setFragmentResultListener( "key", this, new FragmentResultListener() {
+            @Override
+            public void onFragmentResult(@NonNull String requestkey, @NonNull Bundle bundle) {
+                String IngName = bundle.getString(  "perfNombre");
+                String IngEdad = bundle.getString( "perfEdad");
+                String IngSexo = bundle.getString("perfSexo");
+                String IngCiudad = bundle.getString( "perfCiudad");
+                String IngPensamiento = bundle.getString( "perfPensamiento");
+
+                perf_nombre.setText(IngName);
+                perf_edad.setText(IngEdad);
+                perf_sexo.setText(IngSexo);
+                perf_ciudad.setText(IngCiudad);
+                perf_pensamiento.setText(IngPensamiento);
+            }
+        });
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInsatanceState) {
+        return inflater.inflate(R.layout.fragment_ver_perfil, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        perf_nombre = view.findViewById(R.id.IngNombre);
+        perf_edad = view.findViewById(R.id.IngEdad);
+        perf_sexo = view.findViewById(R.id.IngSexo);
+        perf_ciudad = view.findViewById(R.id.IngCiudad);
+        perf_pensamiento = view.findViewById(R.id.IngPensamiento);
+
+    }
+
+
+
 
 
 }
+
+
+
+
+
+
+
