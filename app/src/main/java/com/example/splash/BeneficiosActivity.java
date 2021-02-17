5fd04c2ea48a62d2366f3a7b0f4c424055bc5c64
YenniferQuiceno <yennifer.quiceno@hotmail.com>
2021-02-16 22:37:40 -0500

    Yennifer Quiceno: Se crean cada una de las actividades que van asociadas a los botones de beneficios, asi mismo se asocia el menu creado por Jorge con cada una de esas actividades. Por ultimo se programa los botones de la actividad de beneficios para que se dirija a su actividad correspondiente.

--- app/src/main/java/com/example/splash/BeneficiosActivity.java
+++ app/src/main/java/com/example/splash/BeneficiosActivity.java
@@ -2,7 +2,11 @@ package com.example.splash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class BeneficiosActivity extends AppCompatActivity {
 
@@ -10,5 +14,40 @@ public class BeneficiosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beneficios);
+    }
+
+    //Metodos para ir a la Activity de de cada uno de los items de beneficios
+    public void Benitem1 (View view){
+        Intent benitem1 = new Intent (this, activity_benitem1.class);
+        startActivity(benitem1);
+    }
+
+    public void Benitem2 (View view){
+        Intent benitem2 = new Intent (this, activity_benitem2.class);
+        startActivity(benitem2);
+    }
+
+    public void Benitem3 (View view){
+        Intent benitem3 = new Intent (this, activity_benitem3.class);
+        startActivity(benitem3);
+    }
+
+    public void Benitem4 (View view){
+        Intent benitem4 = new Intent (this, activity_benitem4.class);
+        startActivity(benitem4);
+    }
+    public void Benitem5 (View view){
+        Intent benitem5 = new Intent (this, activity_benitem5.class);
+        startActivity(benitem5);
+    }
+
+    public void Benitem6 (View view){
+        Intent benitem6 = new Intent (this, activity_benitem6.class);
+        startActivity(benitem6);
+    }
+
+    public void Benitem7 (View view){
+        Intent benitem7 = new Intent (this, activity_benitem7.class);
+        startActivity(benitem7);
     }
 }
---
+++ app/src/main/java/com/example/splash/activity_benitem1.java
@@ -0,0 +1,50 @@
+package com.example.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class activity_benitem1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_benitem1);
+    }
+
+    //Metodo para mostrar los botones de accion
+    public boolean onCreateOptionsMenu (Menu menu){
+        getMenuInflater().inflate(R.menu.menuacciones, menu);
+        return true;
+    }
+
+    //Metodo para agregar acciones basicas a los botones
+
+    public boolean onOptionsItemSelected (MenuItem item){
+        int id = item.getItemId();
+
+        if(id == R.id.itemcompartir){
+            Intent regresar = new Intent(this, HomeActivity.class);
+            startActivity(regresar);
+            return true;
+        }
+        else
+        if(id == R.id.itemdescargar){
+            Intent regresar = new Intent(this, HomeActivity.class);
+            startActivity(regresar);
+            return true;
+        }
+        else
+        if(id == R.id.itemhome) {
+            Intent regresar = new Intent(this, HomeActivity.class);
+            startActivity(regresar);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
