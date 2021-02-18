1ab5f153cd14048f854c35d190bcea4d192bf41e
DeinerA <deiner.ariza@globant.com>
2021-02-17 22:03:40 -0500

    Acople de modulo lugares a proyecto principal y configuracion de funcionamiento de del llamado API a google maps - Deiner Ariza

--- app/src/main/java/com/example/splash/HomeActivity.java
+++ app/src/main/java/com/example/splash/HomeActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
+import com.example.splash.ui.MapsActivityLugares;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -12,7 +15,18 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+
+        
+        
+        
+
+      }
+
+    private void StartActivity(Intent intent) {
     }
+
     //Metodo para ir a la actvity informacion
     public void Informacion(View view){
         Intent informacion = new Intent(this, InfoActivity.class);
@@ -24,4 +38,19 @@ public class HomeActivity extends AppCompatActivity {
         Intent beneficios = new Intent (this, BeneficiosActivity.class);
         startActivity(beneficios);
             }
+
+    //Metodo para ir a la Activity Lugares
+    public void Lugares (View view) {
+        Intent lugares = new Intent(this, activity_lugares.class);
+        startActivity(lugares);
+
+    }
+
+
+
+
+
+
+
+
 }
