3f963db887d1dcc83f85b353c3c38bda5f314e4e
wmartinezca <70444529+wmartinezca@users.noreply.github.com>
2021-02-20 20:30:29 -0500

    Creación botones fragment_mi_ayuda - William

--- app/src/main/java/com/example/splash/MiAyuda.java
+++ app/src/main/java/com/example/splash/MiAyuda.java
@@ -2,18 +2,18 @@ package com.example.splash;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MiAyuda#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MiAyuda extends Fragment {
+    Button btnComoDescargar, btnComoCompartirr, btnComoCambiarPerfil;
 
     public MiAyuda() {
         // Required empty public constructor
@@ -26,8 +26,19 @@ public class MiAyuda extends Fragment {
 
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnComoDescargar:
+                break;
+            case R.id.btnComoCompartirr:
+                break;
+            case R.id.btnComoCambiarPerfil:
+                break;
+        }
 
     }
 
