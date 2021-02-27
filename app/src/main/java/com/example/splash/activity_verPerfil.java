7cad696f0e7b4ef995fab69f75921e80a45f5bc9
wmartinezca <70444529+wmartinezca@users.noreply.github.com>
2021-02-27 09:51:14 -0500

    se ajusta la vista en ver perfil despues de pasar datos en modificar perfil - William

--- app/src/main/java/com/example/splash/activity_verPerfil.java
+++ app/src/main/java/com/example/splash/activity_verPerfil.java
@@ -2,13 +2,38 @@ package com.example.splash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class activity_verPerfil extends AppCompatActivity {
+    TextView perf_nombre, perf_edad, perf_sexo, perf_ciudad, perf_pensamiento;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ver_perfil);
+
+        perf_nombre = (TextView)findViewById(R.id.perf_nombre) ;
+        perf_edad = (TextView)findViewById(R.id.perf_edad) ;
+        perf_sexo = (TextView)findViewById(R.id.perf_sexo) ;
+        perf_ciudad = (TextView)findViewById(R.id.perf_ciudad) ;
+        perf_pensamiento = (TextView)findViewById(R.id.perf_pensamiento) ;
+
+
+        String midato, midato1, midato2, midato3, midato4;
+        Intent intent = getIntent();
+
+        midato = intent.getStringExtra("dato");
+        midato1 = intent.getStringExtra("dato1");
+        midato2 = intent.getStringExtra("dato2");
+        midato3 = intent.getStringExtra("dato3");
+        midato4 = intent.getStringExtra("dato4");
+
+        perf_nombre.setText(midato);
+        perf_edad.setText(midato1);
+        perf_sexo.setText(midato2);
+        perf_ciudad.setText(midato3);
+        perf_pensamiento.setText(midato4);
     }
 }
