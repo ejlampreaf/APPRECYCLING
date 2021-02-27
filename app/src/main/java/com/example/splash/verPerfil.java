187cfc871ec08ea86e0c9066580f01874168cc7a
wmartinezca <70444529+wmartinezca@users.noreply.github.com>
2021-02-27 11:21:19 -0500

    Inclusion de opcion foto en ver perfil - William

--- app/src/main/java/com/example/splash/verPerfil.java
+++ app/src/main/java/com/example/splash/verPerfil.java
@@ -1,29 +1,43 @@
 package com.example.splash;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentResultListener;
 
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.dynamic.IFragmentWrapper;
 
 
 public class verPerfil extends AppCompatActivity {
 
     private TextView perf_nombre, perf_edad, perf_sexo, perf_ciudad, perf_pensamiento;
 
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ver_perfil);
-
-        
 
         perf_nombre = (TextView)findViewById(R.id.perf_nombre) ;
         perf_edad = (TextView)findViewById(R.id.perf_edad) ;
@@ -42,6 +56,10 @@ public class verPerfil extends AppCompatActivity {
         String dato4 = getIntent().getStringExtra("dato4") ;
         perf_pensamiento.setText(dato4);
     }
+
+
+
+
 
 
 }
