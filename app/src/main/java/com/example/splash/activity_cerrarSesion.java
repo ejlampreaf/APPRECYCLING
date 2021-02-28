c2bc959ac351180b67b52b97d0f0050683bac349
wmartinezca <70444529+wmartinezca@users.noreply.github.com>
2021-02-28 10:38:54 -0500

    se ajuta  opcion cerrar sesion en el menu - William

--- app/src/main/java/com/example/splash/activity_cerrarSesion.java
+++ app/src/main/java/com/example/splash/activity_cerrarSesion.java
@@ -2,13 +2,36 @@ package com.example.splash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class activity_cerrarSesion extends AppCompatActivity {
+
+    private Button mcerrarSesion;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cerrar_sesion);
+
+        mAuth = FirebaseAuth.getInstance();
+        mcerrarSesion =(Button) findViewById(R.id.cerrarSesion);
+        mcerrarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                Toast.makeText(getApplicationContext(), "Cerrando sesión.", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(activity_cerrarSesion.this, Loginactivity.class));
+                finish();
+            }
+        });
     }
+
+
 }
