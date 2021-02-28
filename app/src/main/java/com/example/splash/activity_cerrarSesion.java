e3dd84cfe47f84aacb22980af81c59dae853e9e1
wmartinezca <70444529+wmartinezca@users.noreply.github.com>
2021-02-28 11:46:46 -0500

    se corrige un error en la  opcion cerrar sesion en el menu - William

--- app/src/main/java/com/example/splash/activity_cerrarSesion.java
+++ app/src/main/java/com/example/splash/activity_cerrarSesion.java
@@ -12,26 +12,20 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class activity_cerrarSesion extends AppCompatActivity {
 
-
     private Button mcerrarSesion;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cerrar_sesion);
 
-
         mcerrarSesion =(Button) findViewById(R.id.cerrarSesion);
         mcerrarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_MAIN);
-                intent.addCategory(Intent.CATEGORY_HOME);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
+                Intent intent = new Intent(activity_cerrarSesion.this, Loginactivity.class);
 
-
+                finish();
 
             }
         });
