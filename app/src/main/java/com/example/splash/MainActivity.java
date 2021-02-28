c33ab69ee807570a24a48db3d2c35c96423e95cc
wmartinezca <70444529+wmartinezca@users.noreply.github.com>
2021-02-28 16:05:05 -0500

    se corrige error en ver perfil - William

--- app/src/main/java/com/example/splash/MainActivity.java
+++ app/src/main/java/com/example/splash/MainActivity.java
@@ -27,12 +27,6 @@ import java.util.TimerTask;
 
 public class MainActivity extends Activity {
 
-    ImageView picture;
-    ImageButton openCamera;
-
-    private static final int REQUEST_PERMISSION_CAMERA = 100;
-    private static final int REQUEST_IMAGE_CAMERA = 101;
-
 
 
     @Override
@@ -59,59 +53,8 @@ public class MainActivity extends Activity {
         Timer tiempo = new Timer();
 
         tiempo.schedule(tarea, 4000);
-
-
-
-        picture = findViewById(R.id.picture);
-        openCamera = findViewById(R.id.btnOpenCamera);
-        openCamera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)== PackageManager.PERMISSION_GRANTED) {
-
-                    }else {
-                        ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CAMERA},REQUEST_PERMISSION_CAMERA);
-                    }
-
-                }else {
-                    goToCamera();
-                }
-            }
-        });
-
-
-    }
-    public void onRequestPermissionResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == REQUEST_PERMISSION_CAMERA) {
-            if (permissions.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                goToCamera();
-            }else {
-                Toast.makeText(this, "no tiene permisos", Toast.LENGTH_LONG).show();
-            }
-        }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAMERA) {
-            if (resultCode == Activity.RESULT_OK){
-                Bitmap bitmap = (Bitmap) data.getExtras().get("data");
-                picture.setImageBitmap(bitmap);
-                Log.i("TAG", "Result=>" + bitmap);
-            }
-        }
-        super.onActivityResult(requestCode, resultCode, data);
-    }
-
-
-    private void goToCamera(){
-        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if(cameraIntent.resolveActivity(getPackageManager())!=null){
-            startActivityForResult(cameraIntent,REQUEST_IMAGE_CAMERA);
-        }
-    }
 
     //Metodo para asignar las funciones correspondientes a las opciones
     public boolean onOptionsItemSelected(MenuItem item){
